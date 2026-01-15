9ca89e5c25f3ba8641679baae6baaaf1305cb417
Hyune-c <choi8608@gmail.com>
2026-01-16 01:34:57 +0900

    feat: Full-Text Search 스키마 초기화 구현

--- docker/init/init.sql
+++ docker/init/init.sql
@@ -4,3 +4,11 @@ CREATE DATABASE rag_db;
 -- rag_db에 pgvector extension 활성화
 \c rag_db
 CREATE EXTENSION IF NOT EXISTS vector;
+
+-- Full-Text Search 트리거 함수 (앱에서 트리거 생성 시 사용)
+CREATE OR REPLACE FUNCTION update_content_tsv() RETURNS TRIGGER AS $$
+BEGIN
+  NEW.content_tsv := to_tsvector('simple', NEW.content);
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
